80eb37e7c317029b96fd30f5cb0f431f69e068bf
李影 ElisabethLi <1213123870@qq.com>
2021-07-25 23:05:36 +0800

    表单简化代码

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import image5 from './5.jpg'
+import image6 from './6.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <h1>Hi, I'm Jeremy!<br />
+        I'm a <b>UI developer</b>,<br />
+        <b>photographer</b> and <b>artist</b> <br /> in Sydney, Australia</h1>
+      <ul>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span>Current</span>
+          <a href='#'>Bupa</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <span>Previous</span>
+          <a href='#'>Oneflare,</a>
+          <a href='#'>Jaxsta,</a>
+          <a href='#'>We Are Social,</a>
+          <a href='#'>Essential</a>
+          <a href='#'>Ansarda,</a>
+        </p>
+      </ul>
+      <ul className='links'>
+        <a href='#'>GITHUB</a>
+        <a href='#'>LINKEDIN</a>
+        <a href='#'>INSTAGRAM</a>
+        <a href='#'>TWITTER</a>
+      </ul>
     </div>
   );
 }
