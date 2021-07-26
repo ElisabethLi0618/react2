330842a313ace381fa0a8099b3749c4e48882414
李影 ElisabethLi <1213123870@qq.com>
2021-07-26 19:55:17 +0800

    表单添加内容完成

--- src/index.js
+++ src/index.js
@@ -1,27 +1,89 @@
-//import { render } from '@testing-library/react';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
+
 
 //表单处理 非受控组件
 class App extends React.Component {
-  constructor(){
-    super()
-    //创建ref
-    this.txtRef = React.createRef()
+  // 初始化状态
+  state ={
+    comments:[
+      { id:1, name:'jack',content:'沙发！！！'},
+      { id:2, name:'rose',content:'楼主好帅'},
+      { id:3, name:'tom',content:'小板凳'}
+    ],
+
+    // 准备状态  评论人
+    userName:'',
+    
+    //准备状态  评论内容
+    userContent:''
   }
-  //获取文本框的值
-  getTxt=()=> {
-    console.log('文本框值为： ',this.txtRef.current.value);
+  // 渲染评价列表：
+  renderList(){
+    // if else 作为判断
+    if(this.state.comments.length === 0){
+      return <div className="no-comment">暂无评论，快去评论吧</div>
+    }
+    return(
+      <ul>
+        {this.state.comments.map(item =>(
+          <li key={item.id}>
+            <h3>评论人：{item.name} </h3>
+            <p>评论内容:{item.content} </p>
+          </li>
+        ))}
+      </ul>
+    )
   }
 
+  //处理表单元素值
+  handleForm = (e) => {
+    const { name, value } = e.target
+    this.setState({
+      [name]:value
+    })
+  }
+
+  //发表评论
+  addComment =() => {
+    const{ comments,userName, userContent } = this.state
+    const newComments =[
+      {
+        id: Math.random(),
+        name:userName,
+        content:userContent
+      },
+      ...comments
+    ]
+
+    //console.log(newComments);
+    this.setState({
+      comments:newComments
+    })
+  }
+  
   render(){
+    const {userName, userContent } = this.state
     return(
-      <div>
-        <input type="text" ref={this.txtRef} />
-        <button onClick={this.getTxt}>获取文本框的值</button>
+      <div className="App">
+        <div>
+          <input className="user" type="text"  placeholder="请输入评论人" value={userName} name="userName" onChange={this.handleForm} />
+          <br />
+          <textarea
+           className="content"
+           cols="30"
+           rows="10"
+           placeholder="请输入评论内容" 
+           value={userContent}
+           name="userContent"
+           onChange={this.handleForm}
+           />
+           <br />
+           <button onClick={this.addComment}>发表评论</button>
+        </div>
+        {/* 通过条件渲染 决定渲染什么内容 */}
+        {this.renderList()}
       </div>
     )
   }
@@ -31,7 +93,3 @@ class App extends React.Component {
 ReactDOM.render(<App />, document.getElementById('root'))
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
