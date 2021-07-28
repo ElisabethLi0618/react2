6ad1f199f700b4d12051ec017b710f87796a9e19
李影 ElisabethLi <1213123870@qq.com>
2021-07-28 18:37:27 +0800

    p49 props基础特点

--- src/index.js
+++ src/index.js
@@ -20,8 +20,13 @@ import ReactDOM from 'react-dom';
  
 //也是先接收数据
 class Hello extends React.Component {
+    //推荐使用props作为的 constrctor 参数
+    constructor(props){
+        super(props)
+        console.log(props);
+    }
     render(){
-        //console.log(this.props)
+        console.log('render', this.props)
         return(
             <div>
                 <h1>props:{this.props.name}</h1>
@@ -31,6 +36,12 @@ class Hello extends React.Component {
 }
 
 //这里是传递数据的
-ReactDOM.render(<Hello name="rose" age={18} />, document.getElementById('root'))
+ReactDOM.render(<Hello 
+    name="rose" 
+    age={18} 
+    colors={['red','green', 'blue']}
+    fn={() => console.log('这是一个函数')}
+    tag={ <p>这是一个标签</p> }
+    />, document.getElementById('root'))
 
 
