fc0433cb66c47c6a0730e1342544a3dabcafb6b8
李影 ElisabethLi <1213123870@qq.com>
2021-08-01 11:40:44 +0800

    p23-37复习代码

--- src/index.js
+++ src/index.js
@@ -4,41 +4,146 @@ import ReactDOM from 'react-dom';
 
 // react 复习
 
-// 1，列表渲染
-
-// const songs = [
-//     {id: 1, name: '莫扎特'},
-//     {id: 2, name: '贝多芬'},
-//     {id: 3, name: '我'},
-// ]
-
-// const list =(
-//     <ul>
-//         {songs.map(item =><li key={item.id}>{item.name}</li> )}
-//     </ul>
-// )
-
-// 2，函数组件
-// function Ying (){
-//     return (
-//         <div>这是一个函数组件，函数名首字母要大写（Y），还要有返回值 （return）</div>
-//     )
-// }
-
-// 3, 箭头函数
-//const Ying =() => <div>这是一个函数组件，函数名首字母要大写（Y），还要有返回值 （return）</div>
-
-// 4,类组件class
+// 1,事件绑定
+//  1，1 类组件绑定事件
 // class Ying extends React.Component {
+//     // 事件处理程序
+//     handleClick(){
+//         console.log('点击事件触发了');
+//     }
 //     render(){
-//         return <div>这是一个名为Ying 的类组件，类组件必须要提供“render（）方法”，也必须有返回值。</div>
+//         return(
+//             <button onClick={this.handleClick}>点一下</button>
+//         )
 //     }
 // }
 
-class Ying extends React.Component {
+
+// 1，2 函数组件绑定事件
+// function App (){
+    
+//     function handleClick(){
+//         console.log('函数组件的事件绑定，事件触发了');
+//     }
+//     return(
+//         // 函数组件没有this 所以只需要把函数名写到花括号里就好
+//         <button onClick={handleClick} >点击</button>
+//     )
+// }
+
+// 2，React 事件对象
+// class App extends React.Component{
+//     handleClick(e){
+//         // 阻止浏览器的默认行为
+//         e.preventDefault()
+//         console.log('a标签的单击事件触发了');
+//     }
+//     render(){
+//         return(
+//             <a href="http://itcast.cn/" onClick={this.handleClick} >传智传媒</a>
+//         )
+//     }
+// }
+
+// 3，state 的基本使用
+
+// class Ying extends React.Component {
+//     state={
+//         count: 0
+//     }
+//     render(){
+//         return(
+//             <div>计数器：{this.state.count} </div>
+//         )
+//     }
+// }
+
+// 3,1 setState 的使用
+// class Ying extends React.Component{
+//      //state 初始状态
+//      state={
+//          count: 0
+//      }
+// 事件处理程序
+//onIncrement =()=>{
+//     this.setState({
+//         count: this.state.count + 1     
+//     })
+// } 和下面不用箭头效果一样
+
+// onIncrement(){
+//     this.setState({
+//         count: this.state.count + 1     
+//     })
+// }
+//     render(){
+//         return(
+//             <div>
+//             <h1>计数器：{this.state.count} </h1>
+//             <button onClick={()=> this.onIncrement()} >+1</button>
+//             </div>
+            
+//         )
+//     }
+// }
+
+//setState 的正确用法
+// this.setState({
+//     count: this.state.count +1
+// })
+
+// 4，手控组件 受到React控制的表单元素  操作文本框的值：
+// class Ying extends React.Component{
+//     state={
+//         txt:''
+//     }
+//     handleChange = e => {
+//         this.setState({
+//             txt: e.target.value
+//         })
+//     }
+//     render(){
+//         return(
+//             <div>
+//                 <input type="text" value={this.state.txt}  onChange ={this.handleChange} />
+//             </div>
+//         )
+//     }
+// }
+
+class Ying extends React.Component{
+    state={
+        txt: '',
+        content: '',
+        city: 'bj',
+        isChecked: false
+    }
+    handleForm = e =>{
+        const target = e.target
+        const value = target.type === 'checkbox'
+        ? target.checked
+        : target.value
+        const name = target.name
+        this.setState({
+            [name]:value
+        })
+    }
+
     render(){
-        return  (
-            <h1>这是一个类组件。</h1>
+        return(
+            <div>
+                <input type="text" name="txt" value={this.state.txt} onChange={this.handleForm} />
+                <br />
+                <textarea name="content" value={this.state.content} onChange={this.handleForm}/>
+                <br />
+                <select name="city" value={this.state.city} onChange={this.handleForm}>
+                    <option value="sh">上海</option>
+                    <option value="bj">北京</option>
+                    <option value="hz">杭州</option>
+                </select>
+                <br />
+                <input type="checkbox" name="isChecked" checked={this.state.isChecked} onChange={this.handleForm} />
+            </div>
         )
     }
 }
